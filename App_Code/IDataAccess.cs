3f0df1863e254129b3369e472544ab006a461034
Adnan Shaheen <shaheen4qau@gmail.com>
2015-10-23 12:02:26 -0400

    adding remainder code

---
+++ App_Code/IDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for IDataAccess
+/// </summary>
+public interface IDataAccess
+{
+	object GetSingleAnswer(string sql,List<SqlParameter> PList);
+    DataTable GetDataTable(string sql, List<SqlParameter> PList);
+    int InsOrUpdOrDel(string sql, List<SqlParameter> PList);
+}
